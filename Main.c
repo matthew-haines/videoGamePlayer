06dd0d6403c22863e4b0d7e0411cf96e89cfff99
alvind1 <40304595+alvind1@users.noreply.github.com>
2019-01-09 13:36:54 -0500

    Update main.c

--- Main.c
+++ Main.c
@@ -18,12 +18,4 @@ typedef struct {
 } user;
 
 int main() {
-    char salt[salt_len];
-    generate_salt(salt);
-    char pass[8] = "pa";
-    char to_be_hashed[16];
-    concatenate(salt, pass, to_be_hashed);
-
-    printf("%X", hash(to_be_hashed));
-    //COMMENT IT MATT
 }
