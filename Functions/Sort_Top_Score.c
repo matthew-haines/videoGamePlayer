3acd9b72f8120c6db8428b3123ea41d732f17edc
alvind1 <40304595+alvind1@users.noreply.github.com>
2019-01-10 09:31:51 -0500

    Update Sort_Top_Score.c

--- Functions/Sort_Top_Score.c
+++ Functions/Sort_Top_Score.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <time.h>
 
+typedef struct {
+    int mth;
+    int dy;
+    int yr;
+} dob;
+
+typedef struct {
+    char usrnm[10];
+    int pswrd;
+    double salt;
+    dob birth;
+    int tpSc;
+    int fre;
+} user;
+
+void sort_ts(){
+
+}
+
+int main() {
+    user fle[101];
+    srand(time(NULL));
+
+    return 0;
+}
