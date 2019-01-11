aac8d516ee809cd05cd17a4bc70f210a320aa1f1
alvind1 <40304595+alvind1@users.noreply.github.com>
2019-01-10 22:22:22 -0500

    Update Main.c

--- Main.c
+++ Main.c
@@ -3,27 +3,9 @@
 #include <stdint.h>
 #include <string.h>
 #include <time.h>
-
-typedef struct {
-    int mnth;
-    int dy;
-    int yr;
-} dte;
-
-typedef struct {
-    char usrnm[10];
-    int pswrd;
-    double salt;
-    //dob birth;
-    struct tm *t;
-    int tpSc;
-    int fre; //1 if spot is free, 0 if taken
-    int dys_old; //days old for comparison (not accurate)
-} user;
+#include "structure.h"
+#include "header.h" //placement name for function header
 
 int main() {
-    user fle[101];
-    srand(time(NULL));
-
     return 0;
 }
