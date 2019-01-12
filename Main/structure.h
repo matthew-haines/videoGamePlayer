1bc6ec9ac236cd403c91a9525dc1c27cee792f70
alvind1 <40304595+alvind1@users.noreply.github.com>
2019-01-12 13:38:56 -0500

    Update structure.h

--- Main/structure.h
+++ Main/structure.h
@@ -8,7 +8,7 @@ typedef struct {
     char usrnm[10];
     int pswrd;
     double salt;
-    dob birth;
+    dte birth;
     int tpSc;
     int fre; //1 if spot is free, 0 if taken
     int dys_old; //days old for comparison (not accurate)
