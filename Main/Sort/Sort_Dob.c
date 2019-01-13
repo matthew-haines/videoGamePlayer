f3c46216ac0ce8a991a7ebb406f51c8526c3f621
alvind1 <40304595+alvind1@users.noreply.github.com>
2019-01-13 15:18:15 -0500

    Update Sort_Dob.c

--- Main/Sort/Sort_Dob.c
+++ Main/Sort/Sort_Dob.c
@@ -6,7 +6,8 @@
 
 /***
     Should be done
-    Not tested
+    Test 1 Passed.
+    Needs more testing.
 ***/
 
 typedef struct {
@@ -25,20 +26,6 @@ typedef struct {
     int dys_old; //days old for comparison (not accurate)
 } user;
 
-int f_age(user p){ //finding age in days; works don't touch
-    dte today;
-    int age = 0;
-    if(age += (today.mnth-1)*31+today.dy-(p.birth.mnth-1)*31-p.birth.dy >= 0) age += (today.mnth-1)*31+today.dy-(p.birth.mnth-1)*31-p.birth.dy;
-    else{
-        age += -((today.mnth-1)*31+today.dy-(p.birth.mnth-1)*31-p.birth.dy);
-        today.yr -= 1;
-    }
-
-    age += 365*(today.yr-p.birth.yr);
-    printf("%d %d %d: %d %d %d: %d", today.yr, today.mnth, today.dy, p.birth.yr, p.birth.mnth, p.birth.dy, age);
-    return age;
-}
-
 dte cur(){ //current date
     struct tm *t;
     time_t now;
@@ -47,9 +34,26 @@ dte cur(){ //current date
     return (dte){t->tm_mon+1, t->tm_mday, t->tm_year+1900};
 }
 
-void age_user_ar(int *ar, user *fle, int n){ //Calculates age of user
+void f_age(user *p){ //finding age in days; works don't touch
+    dte today = cur();
+    p->dys_old = 0;
+    printf("%d T%d\n", today.yr, p->fre);
+    if((today.mnth-1)*31+today.dy >= (p->birth.mnth-1)*31+p->birth.dy) p->dys_old += (today.mnth-1)*31+today.dy-(p->birth.mnth-1)*31-p->birth.dy;
+    else{
+        printf("%d T%d\n", today.yr, p->fre);
+        p->dys_old += 31-p->birth.dy + (12-(p->birth.mnth))*31;
+        p->dys_old += (today.mnth-1)*31+today.dy;
+        today.yr -= 1;
+    }
+    printf("%d T%d\n", today.yr, p->fre);
+
+    p->dys_old += 365*(today.yr-p->birth.yr);
+    printf("%d %d %d: %d %d %d: %d\n", today.yr, today.mnth, today.dy, p->birth.yr, p->birth.mnth, p->birth.dy, p->dys_old);
+}
+
+void age_user_ar(user *fle, int n){ //Calculates age of user
     int i;
-    for(i=0; i<n; i++) fle[i].dys_old = f_age(fle[i]);
+    for(i=0; i<n; i++) f_age(&fle[i]);
 }
 
 /*void user_ar(int num, int *ar, user *f, int n){ //Creating an extra array for age or top Score for sorting, no point in doing this
@@ -74,7 +78,7 @@ int dob_partition(user *ar, int l, int h){
     user temp;
 
     for(j=l; j<h; j++){
-        if(ar[j].dys_old<=ar[h].dys_old){
+        if(ar[j].dys_old>=ar[h].dys_old){
             temp = ar[i];
             ar[i] = ar[j];
             ar[j] = temp;
@@ -90,6 +94,23 @@ int dob_partition(user *ar, int l, int h){
 }
 
 int main() {
+    int i;
+    user p[10];
+    for(i=0; i<10; i++) p[i].fre = i;
+    p[0].birth = (dte){6, 20, 1995};
+    p[1].birth = (dte){4, 4, 2002};
+    p[2].birth = (dte){1, 1, 1991};
+    p[3].birth = (dte){12, 31, 1990};
+    p[4].birth = (dte){2, 29, 2000};
+    p[5].birth = (dte){11, 4, 2014};
+    p[6].birth = (dte){1, 1, 1991};
+    p[7].birth = (dte){6, 19, 1995};
+    p[8].birth = (dte){5, 10, 1994};
+    p[9].birth = (dte){12, 12, 1992};
 
+    age_user_ar(p, 10);
+    dob_qcksort(p, 0, 9);
+    //for(i=0; i<10; i++) printf("%d\n", p[i].dys_old);
+    for(i=0; i<10; i++) printf("%d\\%d\\%d %d\n", p[i].birth.mnth, p[i].birth.dy, p[i].birth.yr, p[i].fre);
     return 0;
 }
