ccdc734edfe43e6575ad17515c1244e9f59883ce
alvind1 <40304595+alvind1@users.noreply.github.com>
2019-01-13 19:43:47 -0500

    Update Main.c

--- Main/Main.c
+++ Main/Main.c
@@ -4,6 +4,9 @@
 #include <string.h>
 #include <time.h>
 #include "structure.h"
+#include "Sort_Dob.h"
+#include "Sort_Top_Score.h"
+#include "Sort_Username.h"
 #include "header.h" //placement name for function header
 
 int main() {
