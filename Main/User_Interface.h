c4241b5e19d763f990757125ba6fe7ee5f76e5ca
RaymondR-Wu <46500844+RaymondR-Wu@users.noreply.github.com>
2019-01-23 14:57:50 -0500

    Update

--- Main/User_Interface.h
+++ Main/User_Interface.h
@@ -1,51 +1,51 @@
-int menu_options(user * fle, int * n){
+int menu_options(struct node * Tree){
     int inpt;
     int i;
     printf("Menu Options:\n1: Add random numbers."
-    "\n2: Output Players.\n3: Add Player."
-    "\n4: Delete Player by LoginID"
-	"\n5: Delete Players by Top Score"
-	"\n6: Sort Players by LoginID"
-	"\n7: Sort Players by Date of Birth"
-	"\n8: Sort Players by Top Score"
-	"\n9: Find Players by LoginID"
-	"\n10: Quit\n");
-    scanf(" %d", &inpt);
+           "\n2: Output Players.\n3: Add Player."
+           "\n4: Delete Player by LoginID"
+           "\n5: Delete Players by Top Score"
+           "\n6: Sort Players by LoginID"
+           "\n7: Sort Players by Date of Birth"
+           "\n8: Sort Players by Top Score"
+           "\n9: Find Players by LoginID"
+           "\n10: Quit\n");
+    scanf("%d", &inpt);
+    fflush(stdin);
     switch(inpt){
         case 1:
-			//printf("%d %d", n, *n);
-			//add_rand_player(fle, n);
-			break;
+            add_rand_player(Tree);
+            break;
         case 2:
-			//output(fle, *n);
-			break;
-		case 3:
-			add_user_player(fle, &fle[*n], *n); //works?
-			//printf("A");
-			*n += 1;
-			break;
+            PrintBST(Tree);
+            break;
+        case 3:
+            add_user_player(Tree); //works?
+            break;
         case 4:
-			//del_id();
-			break;
+            del_id(Tree);
+            break;
         case 5:
-			//del_ts();
-			break;
+            del_ts(Tree);
+            break;
         case 6:
-			break;
+            reSort(Tree, compare_usrnm);
+            break;
         case 7:
-			//age_user_ar(fle, *n);
-			break;
+            reSort(Tree,compare_dob);
+            break;
         case 8:
-			break;
+            reSort(Tree,compare_score);
+            break;
         case 9:
-			//find_id();
-			break;
+            find_id(*Tree);
+            break;
         case 10:
-			return -1;
+            return -1;
         default:
-			break;
+            break;
     }
-    system("pause");
-    system("cls");
+      system("pause");
+      system("cls");
     return 1;
 }
