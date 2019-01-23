452e4b8b8e5f5291797833e07f3edd68ab7878e7
RaymondR-Wu <46500844+RaymondR-Wu@users.noreply.github.com>
2019-01-23 14:59:56 -0500

    Update box-muller.h

--- Main/box-muller.h
+++ Main/box-muller.h
@@ -1,13 +1,13 @@
 #include <stdlib.h>
 #include <math.h>
 
-double normal(double mu, double sigma) {
+double normal(double mu, double sigma, int sd) {
     // Performs box-muller transform on uniform random variables and returns
     // random variable in normal distribution with mean mu and standard deviation sigma
-    srand(time(NULL));
+    srand(sd);
     double u1 = (double)rand() / RAND_MAX;
     double u2 = (double)rand() / RAND_MAX;
-
-    double rand_norm = sqrt(-2 * ln(u1)) * cos(2 * M_PI * u2);
+    
+    double rand_norm = sqrt(-2 * log(u1)) * cos(2 * M_PI * u2);
     return rand_norm * sigma + mu;
 }
