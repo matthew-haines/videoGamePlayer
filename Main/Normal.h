c93065a42666c5f7b71002c0604c5e37b23a1276
RaymondR-Wu <46500844+RaymondR-Wu@users.noreply.github.com>
2019-01-25 12:20:13 -0500

    Delete Normal.h

--- Main/Normal.h
+++
@@ -1,9 +0,0 @@
-int haha(char *s){
-    int n;
-    n = strcmp(s, "Raymond");
-    if(n==0){
-        printf("no, that name is sacred. NO ONE CAN USE THAT NAME\n");
-        return 1;
-    }
-    else return 0;
-}
