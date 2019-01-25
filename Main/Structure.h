75a266d3bde9e31b97d23febde9a69450d5feb0c
RaymondR-Wu <46500844+RaymondR-Wu@users.noreply.github.com>
2019-01-25 12:25:04 -0500

    Create Structure.h

---
+++ Main/Structure.h
@@ -0,0 +1,21 @@
+
+typedef struct {
+    int mth;
+    int dy;
+    int yr;
+} dob;
+
+typedef struct {
+    char usrnm[10];
+    char pswrd[10];
+    double salt;
+    dob birth;
+    int tpSc;
+    int fre;
+} user;
+
+struct node{
+    user value;
+    struct node * lc;
+    struct node * rc;
+};
